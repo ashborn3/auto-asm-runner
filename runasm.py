dc125b3f5f3a86a30e668e2f42a7abb9a804727c
ashborn3 <114130969+ashborn3@users.noreply.github.com>
2023-01-22 16:28:28 +0000

    Add files via upload

---
+++ runasm.py
@@ -0,0 +1,22 @@
+from os import system,listdir
+
+dir = listdir()
+
+for i in dir:
+	l = len(i) - 1
+	if i[(l-3):] == ".asm" :
+		file = i[:(l-3)]
+
+print(f"Compiling and linking {file}.asm")
+
+system(f"nasm -f elf64 {file}.asm")
+
+system(f"ld {file}.o -o {file}.out")
+
+print("OUTPUT :\n-")
+
+system(f"./{file}.out")
+
+print(f"-\nRemoving compiled file {file}.o and linked file {file}.out")
+
+system(f"rm {file}.out {file}.o")
